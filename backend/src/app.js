e6e9f8d82275206a816b3e44d57bcb6ad4dbb5db
Muhammad Bilal <mbilal412004@gmail.com>
2026-08-16 15:39:02 +0500

    production fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use('/api/webhooks/clerk', express.raw({ type: 'application/json' }))
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }))
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,4 @@
 import { sendSuccess, sendError } from "../utils/response.js";
-
-
 export const getMeController = (req, res) => {
     return sendSuccess(res, "User fetched successfully", { userId: req.auth?.userId }, 200);
 };
